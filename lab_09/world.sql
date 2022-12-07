424327e48f12c1df83cfe3b5f8f4177bc6b4cad9
Jose Corcuera <jlcorcuera@gmail.com>
2022-12-07 09:22:58 +0100

    Added create database command.

--- lab_09/world.sql
+++ lab_09/world.sql
@@ -14,7 +14,8 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
+create database if not exists world;
+use world;
 --
 -- Table structure for table `city`
 --
