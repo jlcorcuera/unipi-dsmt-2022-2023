341330e2812d1b901eec035eae4d4e6cd9c9eea7
Jose Corcuera <jlcorcuera@gmail.com>
2022-12-14 14:33:36 +0100

    Code tweaked.

--- lab_11/ms-beer/src/main/java/it/unipi/dsmt/microservices/msbeer/rest/BeerRestController.java
+++ lab_11/ms-beer/src/main/java/it/unipi/dsmt/microservices/msbeer/rest/BeerRestController.java
@@ -9,12 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/v1/beer")
 public class BeerRestController {
 
@@ -22,7 +21,7 @@ public class BeerRestController {
     private BeerService beerService;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody ResponseDTO<List<BeerSearchDTO>> search(@RequestParam(name = "name", required = false) String name,
+    public ResponseDTO<List<BeerSearchDTO>> search(@RequestParam(name = "name", required = false) String name,
                                                                   @RequestParam(name = "price-start", required = false) Float priceStart,
                                                                   @RequestParam(name = "price-end", required = false) Float priceEnd) {
         BeerFiltersDTO beerFiltersDTO = BeerFiltersDTO.builder()
@@ -35,7 +34,7 @@ public class BeerRestController {
     }
 
     @GetMapping("/{id}")
-    public @ResponseBody ResponseDTO<BeerDTO> read(@PathVariable("id") Integer id) {
+    public ResponseDTO<BeerDTO> read(@PathVariable("id") Integer id) {
         BeerDTO dto = beerService.loadById(id);
         return new ResponseDTO<BeerDTO>(dto);
     }
