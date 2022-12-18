e7b08c2007c9ba74615ce29a74167ad5cb4d3870
José Luis Corcuera Bárcena <jlcorcuera@gmail.com>
2022-12-18 18:20:05 -0500

    Fixed typo. Added lab_09 solution.

---
+++ lab_09/lab-09-web/src/main/java/it/unipi/dsmt/jakartaee/lab_09/servlets/BeerSearch2Servlet.java
@@ -0,0 +1,41 @@
+package it.unipi.dsmt.jakartaee.lab_09.servlets;
+
+import it.unipi.dsmt.jakartaee.lab_09_ejb_interfaces.dto.BeerDTO;
+import it.unipi.dsmt.jakartaee.lab_09_ejb_interfaces.interfaces.BeerEJB;
+import jakarta.ejb.EJB;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "BeerSearch2Servlet", value = "/BeerSearch2Servlet")
+public class BeerSearch2Servlet extends HttpServlet {
+
+    @EJB(mappedName = "BeerEJBFromMySQL")
+    private BeerEJB beerEJB;
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String searchFilter = request.getParameter("search") != null ? request.getParameter("search") : "";
+        List<BeerDTO> beerDTOList = beerEJB.search(searchFilter);
+        request.setAttribute("beers", beerDTOList);
+        request.setAttribute("search", searchFilter);
+        String targetJSP = "/WEB-INF/jsp/search.jsp";
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(targetJSP);
+        requestDispatcher.forward(request, response);
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+}
